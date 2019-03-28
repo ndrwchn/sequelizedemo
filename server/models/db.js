3b46f450d5aa230619077fa75e105250fe6002ae
Andrew Chen <andrew.chen@scotiabank.com>
2019-03-28 12:50:23 -0400

    dd

--- server/models/db.js
+++ server/models/db.js
@@ -12,10 +12,9 @@ var db        = {};
 //   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 // }
 
-const sequelize = new Sequelize('', '', '',{
-    host: '',
-    dialect: '',
-    operatorsAliases: false
+const sequelize = new Sequelize('slscmm', 'sydneyplus', 'sydv3',{
+  host: 'tocgsls1iv',
+  dialect: 'mssql'
 })
 
 fs
